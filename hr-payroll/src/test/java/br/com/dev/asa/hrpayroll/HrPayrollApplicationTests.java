a4ccba2d5f7dda5247f2cb2a2c4fd60756e33391
Airton Almeida <tom.almeida8@gmail.com>
2022-10-15 17:10:37 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/br/com/dev/asa/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.asa.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
