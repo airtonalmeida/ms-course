7e95a21340babe12c5adeb3f444296eb43a0c171
Airton Almeida <tom.almeida8@gmail.com>
2022-10-16 20:56:32 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/br/com/dev/asa/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/dev/asa/hrpayroll/resources/PaymentResource.java
@@ -1,11 +1,15 @@
 package br.com.dev.asa.hrpayroll.resources;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import br.com.dev.asa.hrpayroll.entities.Payment;
 import br.com.dev.asa.hrpayroll.services.PaymentService;
@@ -17,11 +21,17 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}	
 	
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+		Payment payment = new Payment("João Pedro", new BigDecimal(300), days);
+		return ResponseEntity.ok(payment);
+	}	
 
 }
