35ffab60ee2cf5b5bb51be2eb00fb00b14392de8
Airton Almeida <tom.almeida8@gmail.com>
2022-10-18 15:56:22 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/br/com/dev/asa/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.asa.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
